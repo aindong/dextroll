e8f4a50ea8710dbb4c7f259c367a5512ddb79ba9
Alleo Indong <alleo.indong@gmail.com>
2022-07-26 12:50:33 +0800

    added chain config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,13 @@
 export default {
   deadAddress: '0x0000000000000000000000000000000000000000',
   tokenAddress: process.env.TOKEN_ADDRESS || '0x0000000000000000000000000000000000000000',
-  walletAmount: process.env.WALLET_AMOUNT || 0
+  walletAmount: process.env.WALLET_AMOUNT || 0,
+
+  chain: {
+    rpc: process.env.RPC_URL || 'https://data-seed-prebsc-1-s2.binance.org:8545',
+    network: {
+      name: process.env.NETWORK_NAME || 'BSC_TESTNET',
+      id: process.env.NETWORK_ID || 97
+    }
+  }
 };
