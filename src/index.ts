f3374c24337238da8e24af264ef1ffa5b27b74a8
Alleo Indong <alleo.indong@gmail.com>
2022-07-26 12:50:59 +0800

    initial index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-import Config from '@/config';
+import chalk from 'chalk';
+import { CountWallets } from '@/utils/wallet/count-wallets';
+import Sleep from '@/utils/sleep';
 
 async function main() {
-  console.log('Hello World!');
-  console.log(`Token address: ${Config.tokenAddress}`);
+  console.log(chalk.green('Welcome to DexTrolls! This is a dex bot for the EVM based blockchain.'));
+
+  console.log('Checking for wallets...');
+  await Sleep(1000);
+
+  //
+  const walletsAreReady = await CountWallets();
+  console.log('walletsAreReady', walletsAreReady);
 }
 
 main().catch((error) => {
